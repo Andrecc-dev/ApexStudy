aa9bab6842ab442fe5e8e54433a15d3b238b5610
andrecc-dev <andrecunha.correa@gmail.com>
2026-08-02 11:23:26 -0300

    feat: adição do Firebase como segurança

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVWPzJ37_barsVpZkk53rzHUY1ffCjpQQ",
+  authDomain: "apexstudy-a762a.firebaseapp.com",
+  projectId: "apexstudy-a762a",
+  storageBucket: "apexstudy-a762a.firebasestorage.app",
+  messagingSenderId: "470039488570",
+  appId: "1:470039488570:web:c861c786b7db94b1772d03"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
